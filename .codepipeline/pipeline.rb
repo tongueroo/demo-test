5f2c25b80106a07404681a8d798cda8b62b32b27
Tung Nguyen <tongueroo@gmail.com>
2019-08-10 02:04:21 +0000

    test1

--- .codepipeline/pipeline.rb
+++ .codepipeline/pipeline.rb
@@ -12,6 +12,14 @@ stage "Build" do
   codebuild "demo"
 end
 
-# stage "Deploy" do
-#   codebuild "action4"
-# end
+stage "Approve" do
+  approve "approve me"
+  # approve(
+  #   notification_arn: "xxx",
+  #   custom_data: "Approve deployment",
+  # )
+end
+
+stage "Deploy" do
+  codebuild "demo-deploy"
+end
